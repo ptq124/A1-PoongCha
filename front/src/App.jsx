6825f66087fe0b1beae9ef5c012476314cd27c0e
menuin <menuin@hanyang.ac.kr>
2023-08-02 14:30:23 +0900

    .

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,17 +2,13 @@ import { theme } from "./styles/theme";
 import { styled } from "styled-components";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global-style";
+
 function App() {
-<<<<<<< HEAD
-      console.log("hello");
-  return <>s</>;
-=======
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
     </ThemeProvider>
   );
->>>>>>> 148a4faf34d507d252e065c839bb7585e3f4a708
 }
 
 export default App;
