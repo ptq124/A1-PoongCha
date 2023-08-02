b7168364d11b56e2bbffd296064aa5db48ba4d54
박태규 Park Tae Kyu <ptq124@naver.com>
2023-08-02 14:29:07 +0900

    feat: 폴더추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import { theme } from "./styles/theme";
 import { styled } from "styled-components";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global-style";
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
