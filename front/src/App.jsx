72b787e1e0d47972225f2e302622ad773e2ffa58
박태규 Park Tae Kyu <ptq124@naver.com>
2023-08-03 16:28:47 +0900

    test

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,13 @@
 import { theme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global-style";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Outlet />
     </ThemeProvider>
   );
 }
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/index.jsx";
+import SurveyPage from "./Pages/SurveyPage/index.jsx";
+import EtcPage from "./Pages/EtcPage/index.jsx";
+import EndPage from "./Pages/EndPage/index.jsx";
+import CustomPage from "./Pages/CustomPage/index.jsx";
+import ResultPage from "./Pages/ResultPage/index.jsx";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
@@ -10,20 +17,20 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, path: "/", element: <LandingPage /> },
-      // { path: "/survey", element: <SurveyPage /> },
-      // {
-      //   path: "/etc_end",
-      //   element: <EtcPage />,
-      // },
-      // { path: "/survey_end", element: <EndPage /> },
-      // {
-      //   path: "/custom",
-      //   element: <CustomPage />,
-      // },
-      // {
-      //   path: "/result",
-      //   element: <ResultPage />,
-      // },
+      { path: "/survey", element: <SurveyPage /> },
+      {
+        path: "/etc_end",
+        element: <EtcPage />,
+      },
+      { path: "/survey_end", element: <EndPage /> },
+      {
+        path: "/custom",
+        element: <CustomPage />,
+      },
+      {
+        path: "/result",
+        element: <ResultPage />,
+      },
     ],
   },
 ]);
