8fa032aeefb06c045cd9cf6402312380331b0fbe
menuin <menuin@hanyang.ac.kr>
2023-08-03 14:17:39 +0900

    style : OptionalHeader 버튼 컴포넌트 스타일 적용

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,13 @@
 import { theme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global-style";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header />
     </ThemeProvider>
   );
 }
--- front/src/Components/SurveyOption/index.jsx
+++ front/src/Components/SurveyOption/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { css, styled } from "styled-components";
 import RadioBtn from "../Common/RadioBtn";
-import Check24BlueIcon from "../../assets/icons/check_24_blue.svg";
+import Check24BlueIcon from "../../assets/checkcircle/check-24-blue.svg";
 
 const SurveyOption = ({ label, index, name, selected, onChange }) => {
   return (
@@ -31,9 +31,9 @@ const SurveyOptionLabel = styled.label`
     props.selected
       ? css`
           background-color: ${({ theme }) => theme.color.grey1000};
-          border: 1.5px solid ${({ theme }) => theme.color.primary_default};
           ${({ theme }) => theme.font.Body2_Bold};
           color: ${({ theme }) => theme.color.primary_default};
+          border: 1.5px solid ${({ theme }) => theme.color.primary_default};
         `
       : css`
           background-color: ${({ theme }) => theme.color.grey800};
