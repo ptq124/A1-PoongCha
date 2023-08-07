72a774eebca279cdf313c51687b319a17dcfdf71
menuin <menuin@hanyang.ac.kr>
2023-08-07 16:23:27 +0900

    chore: cleaning

--- front/src/Components/Common/Summary/SelectedItem/SelectedItem.jsx
+++ front/src/Components/Common/Summary/SelectedItem/SelectedItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
-import ImgItem from "../../ImgItem/ImgItem";
+
 const SelectedItem = () => {
   return (
     <Wrapper>
--- front/src/Components/Common/Summary/index.jsx
+++ front/src/Components/Common/Summary/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { styled } from "styled-components";
 import SelectedItem from "./SelectedItem/SelectedItem";
+
 const Summary = () => {
   return (
     <Wrapper>
