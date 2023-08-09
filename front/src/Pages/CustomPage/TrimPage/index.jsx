f6b4fb41c3ae1ad01a8c9e12d8cd127486699551
박태규 Park Tae Kyu <ptq124@naver.com>
2023-08-10 02:00:04 +0900

    feat: 트림선택 360 컴포넌트 개발

--- front/src/Pages/CustomPage/TrimPage/index.jsx
+++ front/src/Pages/CustomPage/TrimPage/index.jsx
@@ -1,26 +1,17 @@
 import React from "react";
 import { styled } from "styled-components";
 import TrimCustomBar from "./TrimCustomBar";
+import CarView from "../../../Components/Custom/CarView";
 
 const TrimPage = () => {
   return (
     <Wrapper>
-      <Component1></Component1>
+      <CarView></CarView>
       <TrimCustomBar />
     </Wrapper>
   );
 };
 
-const Component1 = styled.div`
-  position: fixed;
-
-  width: 71%;
-  height: 100%;
-
-  left: 0;
-
-  background-color: beige;
-`;
 const Wrapper = styled.div`
   display: flex;
   width: 100%;
@@ -31,4 +22,5 @@ const Wrapper = styled.div`
 
   box-sizing: border-box;
 `;
+
 export default TrimPage;
