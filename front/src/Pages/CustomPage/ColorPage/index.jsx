8f9fb0f775f750b18881ce17f4ec037c1f54efa4
박태규 Park Tae Kyu <ptq124@naver.com>
2023-08-10 17:43:36 +0900

    feat: 컬러 선택 페이지 UI 변경

--- front/src/Pages/CustomPage/ColorPage/index.jsx
+++ front/src/Pages/CustomPage/ColorPage/index.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { styled } from "styled-components";
+import CarView from "../../../Components/Custom/CarView";
+import TrimCustomSideBar from "../TrimPage/TrimCustomSideBar";
 
 const ColorPage = () => {
-  return <div>색상 선택</div>;
+  return (
+    <Wrapper>
+      <CarView></CarView>
+      <TrimCustomSideBar />
+    </Wrapper>
+  );
 };
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: flex-end;
+
+  padding-top: 121px;
+
+  box-sizing: border-box;
+`;
 
 export default ColorPage;
