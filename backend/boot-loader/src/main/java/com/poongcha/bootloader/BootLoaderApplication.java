c9bbc23651f5d40b201fdada0c70ad1f9d7077eb
Yunsik-Choi <choiys0311@gmail.com>
2023-08-12 13:04:54 +0900

    build : 멀티 모듈 세팅

---
+++ backend/boot-loader/src/main/java/com/poongcha/bootloader/BootLoaderApplication.java
@@ -0,0 +1,19 @@
+package com.poongcha.bootloader;
+
+import com.poongcha.car.CarApplication;
+import com.poongcha.recommend.RecommendApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class BootLoaderApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .parent(BootLoaderApplication.class)
+                .child(CarApplication.class, RecommendApplication.class)
+                .run(args);
+    }
+
+}
