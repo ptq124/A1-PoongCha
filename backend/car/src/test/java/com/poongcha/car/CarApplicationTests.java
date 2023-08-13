f61adb96bf0eda37be61cfe42b4381880088281f
Yunsik-Choi <choiys0311@gmail.com>
2023-08-13 22:33:45 +0900

    🏗 build : 차량 모듈 환경 분리

--- backend/car/src/test/java/com/poongcha/car/CarApplicationTests.java
+++ backend/car/src/test/java/com/poongcha/car/CarApplicationTests.java
@@ -2,7 +2,9 @@ package com.poongcha.car;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class CarApplicationTests {
 
