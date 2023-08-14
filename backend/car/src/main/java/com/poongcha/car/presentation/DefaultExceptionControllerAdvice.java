7b4302ad8d71325b8fbad16604aa93a7da37a29f
Yunsik-Choi <choiys0311@gmail.com>
2023-08-14 16:51:30 +0900

    ⚡feat : 외래키 제약조건 어길시 예외 처리 로직 구현

--- backend/car/src/main/java/com/poongcha/car/presentation/DefaultExceptionControllerAdvice.java
+++ backend/car/src/main/java/com/poongcha/car/presentation/DefaultExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.poongcha.car.presentation;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class DefaultExceptionControllerAdvice {
-    @ExceptionHandler(IllegalArgumentException.class)
-    public void invalidRequest(HttpServletResponse response) {
+    @ExceptionHandler({IllegalArgumentException.class})
+    public void notFound(HttpServletResponse response) {
         response.setStatus(HttpStatus.NOT_FOUND.value());
+    }
+
+    @ExceptionHandler({SQLIntegrityConstraintViolationException.class})
+    public void badRequest(HttpServletResponse response) {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
     }
 }
