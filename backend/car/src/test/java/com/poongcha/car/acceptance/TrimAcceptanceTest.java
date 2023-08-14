e370c11cb910f484d173a0844514b2489091193b
Yunsik-Choi <choiys0311@gmail.com>
2023-08-14 16:51:06 +0900

    ⛑️ test : 존재하지 않는 차종으로 트림 생성 테스트 작성

--- backend/car/src/test/java/com/poongcha/car/acceptance/TrimAcceptanceTest.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/TrimAcceptanceTest.java
@@ -1,6 +1,8 @@
 package com.poongcha.car.acceptance;
 
 import static com.poongcha.car.acceptance.CarTypeSteps.차종_생성_요청;
+import static com.poongcha.car.acceptance.TrimSteps.존재하지_않는_차종으로_트림_생성_요청;
+import static com.poongcha.car.acceptance.TrimSteps.존재하지_않는_차종으로_트림_생성_응답_검증;
 import static com.poongcha.car.acceptance.TrimSteps.존재하지_않는_트림_ID_조회_요청;
 import static com.poongcha.car.acceptance.TrimSteps.존재하지_않는_트림_ID_조회_응답_검증;
 import static com.poongcha.car.acceptance.TrimSteps.트림_ID_조회_요청;
@@ -11,8 +13,6 @@ import static com.poongcha.car.acceptance.TrimSteps.트림_생성_응답_검증;
 import com.poongcha.car.util.DocumentationTest;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.apache.http.HttpStatus;
-import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -35,6 +35,25 @@ public class TrimAcceptanceTest extends DocumentationTest {
 
         // THEN
         트림_생성_응답_검증(response, "/api/trim/1");
+    }
+
+    @DisplayName("존재하지 않는 차종으로 트림 생성")
+    @Test
+    void 존재하지_않는_차종으로_트림_생성() {
+        // GIVEN
+        var trimName = "Le Blanc";
+        var imageUrl = "https://www.hyundai.com/contents/vr360/LX06/trim/DS.png";
+        var minPrice = 48_000_000;
+        차종_생성_요청(
+                "palisade",
+                "https://www.hyundai.com/static/images/model/palisade/24my/mo/palisade_highlights_design_m.jpg"
+        );
+
+        // WHEN
+        var response = 존재하지_않는_차종으로_트림_생성_요청(trimName, imageUrl, minPrice, 23819213431358L);
+
+        // THEN
+        존재하지_않는_차종으로_트림_생성_응답_검증(response);
     }
 
     @DisplayName("트림 ID로 조회")
--- backend/car/src/test/java/com/poongcha/car/acceptance/TrimSteps.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/TrimSteps.java
@@ -7,7 +7,6 @@ import static com.poongcha.car.util.DocumentationTest.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -54,6 +53,44 @@ public class TrimSteps {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
             assertions.assertThat(response.header(HttpHeaders.LOCATION)).isEqualTo(location);
+        }
+    }
+
+    public static ExtractableResponse<Response> 존재하지_않는_차종으로_트림_생성_요청(
+            final String trimName,
+            final String imageUrl,
+            final int minPrice,
+            final long carTypeId
+    ) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        customRequestFields(
+                                fieldWithPath("trimName").type(JsonFieldType.STRING).description("트림명"),
+                                fieldWithPath("imageUrl").type(JsonFieldType.STRING).description("트림 이미지 URL"),
+                                fieldWithPath("minPrice").type(JsonFieldType.NUMBER).description("트림 최소 가격"),
+                                fieldWithPath("carTypeId").type(JsonFieldType.NUMBER).description("차종 ID")
+                        )
+                ))
+                .log().all()
+                .when()
+                .body(
+                        Map.of(
+                                "trimName", trimName,
+                                "imageUrl", imageUrl,
+                                "minPrice", minPrice,
+                                "carTypeId", carTypeId
+                        )
+                )
+                .contentType(ContentType.JSON)
+                .post("/api/trim")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 존재하지_않는_차종으로_트림_생성_응답_검증(final ExtractableResponse<Response> response) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
         }
     }
 
