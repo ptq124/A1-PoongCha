4c19ee8f868e59761bbd4ff49b6cc98d4471ac69
Yunsik-Choi <choiys0311@gmail.com>
2023-08-14 15:16:57 +0900

    ⛑️ test : Trim ID로 조회 테스트 작성

--- backend/car/src/test/java/com/poongcha/car/acceptance/TrimSteps.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/TrimSteps.java
@@ -5,8 +5,8 @@ import static com.poongcha.car.util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
 import static com.poongcha.car.util.DocumentationTest.customRequestFields;
 import static com.poongcha.car.util.DocumentationTest.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
-import com.poongcha.car.util.DocumentationTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -20,7 +20,7 @@ public class TrimSteps {
     public static ExtractableResponse<Response> 트림_생성_요청(
             final String trimName,
             final String imageUrl,
-            final int minPrice,
+            final long minPrice,
             final long carTypeId
     ) {
         return given()
@@ -53,6 +53,43 @@ public class TrimSteps {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
             assertions.assertThat(response.header(HttpHeaders.LOCATION)).isEqualTo(location);
+        }
+    }
+
+    public static ExtractableResponse<Response> 트림_ID_조회_요청(final long id) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("트림 ID"),
+                                fieldWithPath("trimName").type(JsonFieldType.STRING).description("트림명"),
+                                fieldWithPath("imageUrl").type(JsonFieldType.STRING).description("트림 이미지 URL"),
+                                fieldWithPath("minPrice").type(JsonFieldType.NUMBER).description("트림 최소 가격"),
+                                fieldWithPath("carTypeId").type(JsonFieldType.NUMBER).description("차종 ID")
+                        )
+                ))
+                .log().all()
+                .when()
+                .get("/api/trim/{id}", id)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 트림_ID_조회_응답_검증(
+            final ExtractableResponse<Response> response,
+            final long trimId,
+            final String trimName,
+            final String imageUrl,
+            final long minPrice,
+            final long carTypeId
+    ) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+            assertions.assertThat(response.jsonPath().getLong("id")).isEqualTo(trimId);
+            assertions.assertThat(response.jsonPath().getString("trimName")).isEqualTo(trimName);
+            assertions.assertThat(response.jsonPath().getString("imageUrl")).isEqualTo(imageUrl);
+            assertions.assertThat(response.jsonPath().getLong("minPrice")).isEqualTo(minPrice);
+            assertions.assertThat(response.jsonPath().getLong("carTypeId")).isEqualTo(carTypeId);
         }
     }
 }
