df28bad368d674277a42b2337460d2e73ce5d829
Yunsik-Choi <choiys0311@gmail.com>
2023-08-14 14:37:48 +0900

    ⚡feat : DataInitializeExecutionListener truncate 외래키 제약조건 수정

--- backend/car/src/test/java/com/poongcha/car/util/DataInitializeExecutionListener.java
+++ backend/car/src/test/java/com/poongcha/car/util/DataInitializeExecutionListener.java
@@ -1,6 +1,7 @@
 package com.poongcha.car.util;
 
 import com.poongcha.car.domain.CarType;
+import com.poongcha.car.domain.Trim;
 import java.sql.PreparedStatement;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.relational.core.mapping.Table;
@@ -14,14 +15,15 @@ public class DataInitializeExecutionListener extends AbstractTestExecutionListen
         ApplicationContext applicationContext = testContext.getApplicationContext();
         NamedParameterJdbcOperations operation = applicationContext.getBean(NamedParameterJdbcOperations.class);
         reset(operation, CarType.class.getAnnotation(Table.class).name());
+        reset(operation, Trim.class.getAnnotation(Table.class).name());
     }
 
     private static void reset(
             final NamedParameterJdbcOperations operation,
             final String tableName
     ) {
-        operation.execute("set @foreign_key_checks = 0;", PreparedStatement::execute);
+        operation.execute("set foreign_key_checks = 0;", PreparedStatement::execute);
         operation.execute(String.format("truncate table %s;", tableName), PreparedStatement::execute);
-        operation.execute("set @foreign_key_checks = 1;", PreparedStatement::execute);
+        operation.execute("set foreign_key_checks = 1;", PreparedStatement::execute);
     }
 }
