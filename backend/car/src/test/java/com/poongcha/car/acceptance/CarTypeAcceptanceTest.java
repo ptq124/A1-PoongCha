02f3393be98a4ee4cb0a8c141af492faef19de0a
Yunsik-Choi <choiys0311@gmail.com>
2023-08-14 09:57:10 +0900

    ⛑️ test : 차종 ID 조회 인수 테스트 작성

--- backend/car/src/test/java/com/poongcha/car/acceptance/CarTypeAcceptanceTest.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarTypeAcceptanceTest.java
@@ -1,9 +1,13 @@
 package com.poongcha.car.acceptance;
 
+import static com.poongcha.car.acceptance.CarTypeSteps.차종_ID_조회_요청;
+import static com.poongcha.car.acceptance.CarTypeSteps.차종_ID_조회_응답_검증;
 import static com.poongcha.car.acceptance.CarTypeSteps.차종_생성_요청;
 import static com.poongcha.car.acceptance.CarTypeSteps.차종_생성_응답_검증;
 
 import com.poongcha.car.util.DocumentationTest;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +25,20 @@ public class CarTypeAcceptanceTest extends DocumentationTest {
 
         // THEN
         차종_생성_응답_검증(response, "/api/car-type/1");
+    }
+
+    @DisplayName("차종 ID 조회")
+    @Test
+    void 차종_ID_조회() {
+        // GIVEN
+        var car_type_name = "palisade";
+        var image_url = "https://www.hyundai.com/static/images/model/palisade/24my/mo/palisade_highlights_design_m.jpg";
+        차종_생성_요청(car_type_name, image_url);
+
+        // WHEN
+        var response = 차종_ID_조회_요청(1L);
+
+        // THEN
+        차종_ID_조회_응답_검증(response, 1L, car_type_name, image_url);
     }
 }
--- backend/car/src/test/java/com/poongcha/car/acceptance/CarTypeSteps.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarTypeSteps.java
@@ -5,6 +5,7 @@ import static com.poongcha.car.util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
 import static com.poongcha.car.util.DocumentationTest.customRequestFields;
 import static com.poongcha.car.util.DocumentationTest.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
@@ -43,6 +44,38 @@ public class CarTypeSteps {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
             assertions.assertThat(response.headers().get(HttpHeaders.LOCATION).getValue()).isEqualTo(expectedLocation);
+        }
+    }
+
+    public static ExtractableResponse<Response> 차종_ID_조회_요청(final long id) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("차종 ID"),
+                                fieldWithPath("carTypeName").type(JsonFieldType.STRING).description("차종명"),
+                                fieldWithPath("imageUrl").type(JsonFieldType.STRING).description("차종 이미지 url")
+                        )
+                ))
+                .log().all()
+                .when()
+                .contentType(ContentType.JSON)
+                .get("/api/car-type/{id}", id)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 차종_ID_조회_응답_검증(
+            final ExtractableResponse<Response> response,
+            final long id,
+            final String carTypeName,
+            final String imageUrl
+    ) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+            assertions.assertThat(response.jsonPath().getLong("id")).isEqualTo(id);
+            assertions.assertThat(response.jsonPath().getString("carTypeName")).isEqualTo(carTypeName);
+            assertions.assertThat(response.jsonPath().getString("imageUrl")).isEqualTo(imageUrl);
         }
     }
 }
