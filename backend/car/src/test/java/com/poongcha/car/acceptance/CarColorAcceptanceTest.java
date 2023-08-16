c2490389c09ba3b8bdf97a0f11944320aaae5a13
Yunsik-Choi <choiys0311@gmail.com>
2023-08-16 17:16:53 +0900

    ⛑️ test : 양립 불가능한 색상 설정 테스트 작성

--- backend/car/src/test/java/com/poongcha/car/acceptance/CarColorAcceptanceTest.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarColorAcceptanceTest.java
@@ -1,5 +1,7 @@
 package com.poongcha.car.acceptance;
 
+import static com.poongcha.car.acceptance.CarColorSteps.양립_불가능한_차량_색상_설정_요청;
+import static com.poongcha.car.acceptance.CarColorSteps.양립_불가능한_차량_색상_설정_응답_검증;
 import static com.poongcha.car.acceptance.CarColorSteps.차량_색상_생성_요청;
 import static com.poongcha.car.acceptance.CarColorSteps.차량_색상_생성_응답_검증;
 
@@ -22,5 +24,25 @@ public class CarColorAcceptanceTest extends DocumentationTest {
 
         // THEN
         차량_색상_생성_응답_검증(response, "/api/color/1");
+    }
+
+    @DisplayName("양립 불가능한 차량 색상 설정")
+    @Test
+    void 양립_불가능한_차량_색상_설정() {
+        // GIVEN
+        차량_색상_생성_요청("red", "www.naver.com/color/red.png", "INTERIOR");
+        차량_색상_생성_요청("green", "www.naver.com/color/green.png", "EXTERIOR");
+        차량_색상_생성_요청("blue", "www.naver.com/color/blue.png", "EXTERIOR");
+        차량_색상_생성_요청("orange", "www.naver.com/color/orange.png", "INTERIOR");
+        var carColorId = 1L;
+        var incompatibleCarColor1 = 2L;
+        var compatibleCarColor = 3L;
+        var incompatibleCarColor2 = 4L;
+
+        // WHEN
+        var response = 양립_불가능한_차량_색상_설정_요청(carColorId, incompatibleCarColor1, incompatibleCarColor2);
+
+        // THEN
+        양립_불가능한_차량_색상_설정_응답_검증(response, "/api/color/1");
     }
 }
--- backend/car/src/test/java/com/poongcha/car/acceptance/CarColorSteps.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarColorSteps.java
@@ -5,6 +5,8 @@ import static com.poongcha.car.util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
 import static com.poongcha.car.util.DocumentationTest.customRequestFields;
 import static com.poongcha.car.util.DocumentationTest.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
@@ -43,6 +45,37 @@ public class CarColorSteps {
     }
 
     public static void 차량_색상_생성_응답_검증(final ExtractableResponse<Response> response, final String location) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
+            assertions.assertThat(response.header(HttpHeaders.LOCATION)).isEqualTo(location);
+        }
+    }
+
+    public static ExtractableResponse<Response> 양립_불가능한_차량_색상_설정_요청(
+            final long carColorId,
+            final long... incompatibleCarColor
+    ) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        pathParameters(
+                                parameterWithName("id").description("차량 색상 ID")
+                        ),
+                        customRequestFields(
+                                fieldWithPath("ids").type(JsonFieldType.ARRAY).description("양립 불가능한 차량 색상 ID")
+                        )
+                )).log().all()
+                .when()
+                .body(Map.of(
+                        "ids", incompatibleCarColor
+                ))
+                .contentType(ContentType.JSON)
+                .post("/api/color/{id}/incompatible", carColorId)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 양립_불가능한_차량_색상_설정_응답_검증(final ExtractableResponse<Response> response, final String location) {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
             assertions.assertThat(response.header(HttpHeaders.LOCATION)).isEqualTo(location);
