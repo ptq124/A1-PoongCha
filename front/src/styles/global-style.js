e7d450cb464c23071c31231464646320ffe30ef8
menuin <menuin@hanyang.ac.kr>
2023-08-16 23:53:00 +0900

    style: 이미지 드래그 안되게 수정, 엔진 바디 구동방식 선택 애니메이션 추가

--- front/src/styles/global-style.js
+++ front/src/styles/global-style.js
@@ -3,4 +3,17 @@ import { reset } from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
+  // 이미지 선택, 드래그 방지
+  img {
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+  user-drag: none;
+}
 `;
