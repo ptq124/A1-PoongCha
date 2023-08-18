41af571eb5612ee8efc64a1deda230b8c9ceb455
Yunsik-Choi <choiys0311@gmail.com>
2023-08-19 01:58:31 +0900

    ⛑️ test : 차량 옵션 그룹 ID로 조회 테스트

--- backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionGroupAcceptanceTest.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionGroupAcceptanceTest.java
@@ -2,11 +2,14 @@ package com.poongcha.car.acceptance;
 
 import static com.poongcha.car.acceptance.CarOptionGroupSteps.존재하지_않는_차량_옵션_ID로_차량_옵션_그룹_생성_요청;
 import static com.poongcha.car.acceptance.CarOptionGroupSteps.존재하지_않는_차량_옵션_ID로_차량_옵션_그룹_생성_응답_검증;
+import static com.poongcha.car.acceptance.CarOptionGroupSteps.차량_옵션_그룹_ID_조회_요청;
+import static com.poongcha.car.acceptance.CarOptionGroupSteps.차량_옵션_그룹_ID_조회_응답_검증;
 import static com.poongcha.car.acceptance.CarOptionGroupSteps.차량_옵션_그룹_생성_요청;
 import static com.poongcha.car.acceptance.CarOptionGroupSteps.차량_옵션_그룹_생성_응답_검증;
 import static com.poongcha.car.acceptance.CarOptionSteps.차량_옵션_생성_요청;
 
 import com.poongcha.car.util.DocumentationTest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -49,29 +52,36 @@ public class CarOptionGroupAcceptanceTest extends DocumentationTest {
     @Test
     void 차량_옵션_그룹_ID_조회() {
         // GIVEN
-        차량_옵션_생성_요청(
-                "후석 승객 알림",
-                "www.naver.com/option/image.png",
-                handleInstallationLocation,
-                "초음파 센서를 통해 뒷좌석에 남아있는 승객의 움직임을 감지하여 운전자에게 경고함으로써 부주의에 의한 유아 또는 반려 동물 등의 방치 사고를 예방하는 신기술입니다."
-        );
-        차량_옵션_생성_요청(
-                "전방 추돌 방지 알림",
-                "www.naver.com/option/image.png",
-                "DEFAULT",
-                "카메라를를 통해 전방 차량을 감지해 추돌을 방지해주는 옵션"
-        );
+        String optionName1 = "후석 승객 알림";
+        String imageUrl1 = "www.naver.com/option/image.png";
+        String detailDescription1 = "초음파 센서를 통해 뒷좌석에 남아있는 승객의 움직임을 감지하여 운전자에게 경고함으로써 부주의에 의한 유아 또는 반려 동물 등의 방치 사고를 예방하는 신기술입니다.";
+        String installationLocation1 = handleInstallationLocation;
+        차량_옵션_생성_요청(optionName1, imageUrl1, installationLocation1, detailDescription1);
+
+        String optionName2 = "전방 추돌 방지 알림";
+        String imageUrl2 = "www.naver.com/option/image.png";
+        String installationLocation2 = "DEFAULT";
+        String detailDescription2 = "카메라를를 통해 전방 차량을 감지해 추돌을 방지해주는 옵션";
+        차량_옵션_생성_요청(optionName2, imageUrl2, installationLocation2, detailDescription2);
+
+        차량_옵션_그룹_생성_요청(compote2CarOptionGroupName, additionalPrice, summaryDescription, new long[]{1L, 2L});
 
         // WHEN
-        var response = 차량_옵션_그룹_생성_요청(
+        var response = 차량_옵션_그룹_ID_조회_요청(1L);
+
+        // THEN
+        차량_옵션_그룹_ID_조회_응답_검증(
+                response,
+                1L,
                 compote2CarOptionGroupName,
                 additionalPrice,
                 summaryDescription,
-                new long[]{1L, 2L}
+                List.of(1, 2),
+                List.of(optionName1, optionName2),
+                List.of(imageUrl1, imageUrl2),
+                List.of(detailDescription1, detailDescription2),
+                List.of(installationLocation1, installationLocation2)
         );
-
-        // THEN
-        차량_옵션_그룹_생성_응답_검증(response, "/api/option-group/1");
     }
 
     @DisplayName("존재하지 않는 차량 옵션 ID로 차량 옵션 그룹 생성")
--- backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionGroupSteps.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionGroupSteps.java
@@ -5,10 +5,14 @@ import static com.poongcha.car.util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
 import static com.poongcha.car.util.DocumentationTest.customRequestFields;
 import static com.poongcha.car.util.DocumentationTest.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.List;
 import java.util.Map;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
@@ -92,6 +96,74 @@ public class CarOptionGroupSteps {
     public static void 존재하지_않는_차량_옵션_ID로_차량_옵션_그룹_생성_응답_검증(final ExtractableResponse<Response> response) {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
+        }
+    }
+
+    public static ExtractableResponse<Response> 차량_옵션_그룹_ID_조회_요청(final long id) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        pathParameters(
+                                parameterWithName("id").description("차량 컴포넌트 그룹 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER)
+                                        .description("차량 옵션 그룹 id"),
+                                fieldWithPath("name").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 그룹 이름"),
+                                fieldWithPath("additionalPrice").type(JsonFieldType.NUMBER)
+                                        .description("차량 옵션 그룹 가격"),
+                                fieldWithPath("summaryDescription").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 그룹 요약 설명"),
+                                fieldWithPath("options").type(JsonFieldType.ARRAY)
+                                        .description("차량 옵션 목록"),
+                                fieldWithPath("options[].id").type(JsonFieldType.NUMBER)
+                                        .description("차량 옵션 id"),
+                                fieldWithPath("options[].name").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 이름"),
+                                fieldWithPath("options[].imageUrl").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 이미지 URL"),
+                                fieldWithPath("options[].installationLocation").type(JsonFieldType.STRING)
+                                        .description("설치 위치"),
+                                fieldWithPath("options[].detailDescription").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 자세한 설명")
+                        )
+                ))
+                .log().all()
+                .when()
+                .get("/api/option-group/{id}", id)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 차량_옵션_그룹_ID_조회_응답_검증(
+            final ExtractableResponse<Response> response,
+            final long optionGroupId,
+            final String carOptionGroupName,
+            final long additionalPrice,
+            final String summaryDescription,
+            final List<Integer> optionIds,
+            final List<String> optionNames,
+            final List<String> imageUrls,
+            final List<String> detailDescriptions,
+            final List<String> installationLocations
+    ) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+            assertions.assertThat(response.jsonPath().getLong("id")).isEqualTo(optionGroupId);
+            assertions.assertThat(response.jsonPath().getString("name")).isEqualTo(carOptionGroupName);
+            assertions.assertThat(response.jsonPath().getLong("additionalPrice")).isEqualTo(additionalPrice);
+            assertions.assertThat(response.jsonPath().getString("summaryDescription")).isEqualTo(summaryDescription);
+            assertions.assertThat(response.jsonPath().getList("options.id")).usingRecursiveComparison()
+                    .isEqualTo(optionIds);
+            assertions.assertThat(response.jsonPath().getList("options.name")).usingRecursiveComparison()
+                    .isEqualTo(optionNames);
+            assertions.assertThat(response.jsonPath().getList("options.imageUrl")).usingRecursiveComparison()
+                    .isEqualTo(imageUrls);
+            assertions.assertThat(response.jsonPath().getList("options.detailDescription")).usingRecursiveComparison()
+                    .isEqualTo(detailDescriptions);
+            assertions.assertThat(response.jsonPath().getList("options.installationLocation"))
+                    .usingRecursiveComparison().isEqualTo(installationLocations);
         }
     }
 }
