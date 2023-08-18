b81708e5b14be23d165d8faa718eb1339a89441a
Yunsik-Choi <choiys0311@gmail.com>
2023-08-18 23:37:38 +0900

    ⛑️ test : 차량 옵션 생성 기능 테스트

---
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionSteps.java
@@ -0,0 +1,54 @@
+package com.poongcha.car.acceptance;
+
+import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
+import static com.poongcha.car.util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
+import static com.poongcha.car.util.DocumentationTest.customRequestFields;
+import static com.poongcha.car.util.DocumentationTest.given;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.Map;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.assertj.core.api.AutoCloseableSoftAssertions;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+public class CarOptionSteps {
+    public static ExtractableResponse<Response> 차량_옵션_생성_요청(
+            final String carOptionName,
+            final String imageUrl,
+            final String detailDescription
+    ) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        customRequestFields(
+                                fieldWithPath("carOptionName").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 이름"),
+                                fieldWithPath("imageUrl").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 이미지 URL"),
+                                fieldWithPath("detailDescription").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 자세한 설명")
+                        )
+                )).log().all()
+                .when()
+                .body(Map.of(
+                        "carOptionName", carOptionName,
+                        "imageUrl", imageUrl,
+                        "detailDescription", detailDescription
+                ))
+                .contentType(ContentType.JSON)
+                .post("/api/option")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 차량_옵션_생성_응답_검증(final ExtractableResponse<Response> response, final String location) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
+            assertions.assertThat(response.header(HttpHeaders.LOCATION)).isEqualTo(location);
+        }
+    }
+}
