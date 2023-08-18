8d5754d23a93aaee844825d563ce2ca5ac110fe0
Yunsik-Choi <choiys0311@gmail.com>
2023-08-19 06:28:14 +0900

    ⛑️ test : 차량 옵션 툴팁 조회 테스트

--- backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionGroupTooltipSteps.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionGroupTooltipSteps.java
@@ -5,6 +5,9 @@ import static com.poongcha.car.util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
 import static com.poongcha.car.util.DocumentationTest.customRequestFields;
 import static com.poongcha.car.util.DocumentationTest.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
@@ -88,6 +91,48 @@ public class CarOptionGroupTooltipSteps {
     public static void 존재하지_않는_차량_옵션_툴팁_생성_응답_검증(final ExtractableResponse<Response> response) {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
+        }
+    }
+
+    public static ExtractableResponse<Response> 차량_옵션_툴팁_ID_조회_요청(
+            final long id,
+            final long carOptionGroupTooltipId
+    ) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        pathParameters(
+                                parameterWithName("id").description("차량 옵션 그룹 ID"),
+                                parameterWithName("tooltip-id").description("차량 옵션 툴팁 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER)
+                                        .description("차량 옵션 툴팁 ID"),
+                                fieldWithPath("imageUrl").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 툴팁 이미지"),
+                                fieldWithPath("tooltipDescription").type(JsonFieldType.STRING)
+                                        .description("차량 옵션 툴팁 설명")
+                        )
+                )).log().all()
+                .when()
+                .pathParam("id", id)
+                .pathParam("tooltip-id", carOptionGroupTooltipId)
+                .get("/api/option-group/{id}/tooltip/{tooltip-id}", id, carOptionGroupTooltipId)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 차량_옵션_툴팁_ID_조회_응답_검증(
+            final ExtractableResponse<Response> response,
+            final long id,
+            final String imageUrl,
+            final String description
+    ) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+            assertions.assertThat(response.jsonPath().getLong("id")).isEqualTo(id);
+            assertions.assertThat(response.jsonPath().getString("imageUrl")).isEqualTo(imageUrl);
+            assertions.assertThat(response.jsonPath().getString("tooltipDescription")).isEqualTo(description);
         }
     }
 }
--- backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionTooltipGroupAcceptanceTest.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarOptionTooltipGroupAcceptanceTest.java
@@ -3,6 +3,8 @@ package com.poongcha.car.acceptance;
 import static com.poongcha.car.acceptance.CarOptionGroupSteps.차량_옵션_그룹_생성_요청;
 import static com.poongcha.car.acceptance.CarOptionGroupTooltipSteps.존재하지_않는_차량_옵션_툴팁_생성_요청;
 import static com.poongcha.car.acceptance.CarOptionGroupTooltipSteps.존재하지_않는_차량_옵션_툴팁_생성_응답_검증;
+import static com.poongcha.car.acceptance.CarOptionGroupTooltipSteps.차량_옵션_툴팁_ID_조회_요청;
+import static com.poongcha.car.acceptance.CarOptionGroupTooltipSteps.차량_옵션_툴팁_ID_조회_응답_검증;
 import static com.poongcha.car.acceptance.CarOptionGroupTooltipSteps.차량_옵션_툴팁_생성_요청;
 import static com.poongcha.car.acceptance.CarOptionGroupTooltipSteps.차량_옵션_툴팁_생성_응답_검증;
 
@@ -14,7 +16,7 @@ import org.junit.jupiter.api.Test;
 public class CarOptionTooltipGroupAcceptanceTest extends DocumentationTest {
     @DisplayName("차량 옵션 툴팁 생성")
     @Test
-    void create() {
+    void 차량_옵션_툴팁_생성() {
         // GIVEN
         차량_옵션_그룹_생성_요청(
                 "차량 옵션 그룹 이름",
@@ -36,7 +38,7 @@ public class CarOptionTooltipGroupAcceptanceTest extends DocumentationTest {
 
     @DisplayName("존재하지 않는 차량 옵션 그룹의 ID로 차량 옵션 툴팁 생성")
     @Test
-    void createIdNotFound() {
+    void 존재하지_않는_차량_옵션_그룹_ID로_차량_옵션_툴팁_생성() {
         // GIVEN
         차량_옵션_그룹_생성_요청(
                 "차량 옵션 그룹 이름",
@@ -54,5 +56,32 @@ public class CarOptionTooltipGroupAcceptanceTest extends DocumentationTest {
 
         // THEN
         존재하지_않는_차량_옵션_툴팁_생성_응답_검증(response);
+    }
+
+    @DisplayName("차량 옵션 툴팁 ID 조회")
+    @Test
+    void create() {
+        // GIVEN
+        차량_옵션_그룹_생성_요청(
+                "차량 옵션 그룹 이름",
+                10_000_000,
+                "차량 옵션 그룹 간단한 설명",
+                new long[]{}
+        );
+        차량_옵션_툴팁_생성_요청(
+                "www.naver.com/tooltip/image.png",
+                "툴팁 설명입니다.",
+                1L
+        );
+
+        var response = 차량_옵션_툴팁_ID_조회_요청(1L, 1L);
+
+        // THEN
+        차량_옵션_툴팁_ID_조회_응답_검증(
+                response,
+                1L,
+                "www.naver.com/tooltip/image.png",
+                "툴팁 설명입니다."
+        );
     }
 }
