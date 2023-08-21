d188031cbf9d87bc6d93e8c7ef53d98cf3d9ead1
Yunsik-Choi <choiys0311@gmail.com>
2023-08-22 08:28:26 +0900

    ⛑️ test : 라이프스타일 퍼소나 전체 조회 테스트

--- backend/recommend/src/test/java/com/poongcha/recommend/acceptance/LifestylePersonaAcceptanceTest.java
+++ backend/recommend/src/test/java/com/poongcha/recommend/acceptance/LifestylePersonaAcceptanceTest.java
@@ -3,6 +3,8 @@ package com.poongcha.recommend.acceptance;
 import static com.poongcha.recommend.acceptance.AdditionalQuestionSteps.추가_질문_생성_요청;
 import static com.poongcha.recommend.acceptance.LifestylePersonaSteps.라이프스타일_퍼소나_생성_요청;
 import static com.poongcha.recommend.acceptance.LifestylePersonaSteps.라이프스타일_퍼소나_생성_응답_검증;
+import static com.poongcha.recommend.acceptance.LifestylePersonaSteps.라이프스타일_퍼소나_전체_조회_요청;
+import static com.poongcha.recommend.acceptance.LifestylePersonaSteps.라이프스타일_퍼소나_전체_조회_응답_검증;
 import static com.poongcha.recommend.acceptance.LifestyleSituationTagSteps.라이프스타일_상황_태그_생성_요청;
 
 import com.poongcha.recommend.util.RecommendAcceptanceTest;
@@ -14,12 +16,6 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("라이프스타일 퍼소나 관련 기능")
 public class LifestylePersonaAcceptanceTest extends RecommendAcceptanceTest {
-    private final int ageAdditionalQuestionId = 1;
-    private final int drivingCareerAdditionalQuestionId = 2;
-    private final int familyMemberAdditionalQuestionId = 3;
-    private final int purposeAdditionalQuestionId = 4;
-    private final int valueAdditionalQuestionId = 5;
-
     @BeforeEach
     void setUp() {
         추가_질문_생성_요청("나이", "나이를 알려주세요.", List.of("20대", "30대", "40대", "50대 이상"));
@@ -53,11 +49,84 @@ public class LifestylePersonaAcceptanceTest extends RecommendAcceptanceTest {
                                 "answer",
                                 "저는 차를 살 때 안전을 중요하게 생각해요. 가족들이 같이 타는 차라 항상 사고에 경각심을 갖고 있죠. 펠리세이드는 그 점에서 뒷좌석 에어백도 터지는 모델이라 안심이 되는 편이에요."
                         )),
-                List.of(1L, 5L, 19L),
-                List.of(1L, 2L)
+                List.of(1, 5, 19),
+                List.of(1, 2)
         );
 
         // THEN
         라이프스타일_퍼소나_생성_응답_검증(response, "/lifestyle-persona/1");
+    }
+
+
+    @DisplayName("라이프스타일 퍼소나 전체 조회")
+    @Test
+    void 라이프스타일_퍼소나_전체_조회() {
+        // GIVEN
+        String representativePhrase1 = "가족과 함께 타서 안전을 중시해요.";
+        String profileImageUrl1 = "www.naver.com/profile/image.png";
+        String profileName1 = "김현대";
+        String profileIntroduction1 = "우리 아이들과 함께 타는 차는 항상 안전해야 한다고 생각해요.";
+        String coverImageUrl1 = "www.naver.com/cover/image.png";
+        List<Integer> additionalQuestionOptionIds1 = List.of(1, 5, 19);
+        List<Integer> lifestylePersonaSituationTagIds1 = List.of(1, 2);
+        라이프스타일_퍼소나_생성_요청(
+                representativePhrase1,
+                profileImageUrl1,
+                profileName1,
+                profileIntroduction1,
+                coverImageUrl1,
+                List.of(
+                        Map.of(
+                                "question", "어떤 용도로 차를 사용하세요?",
+                                "answer", "저는 차를 타고 출퇴근도 하지만 주중에 아이들 픽업하거나 마트 갈 때도 자주 타곤 합니다."
+                        ),
+                        Map.of(
+                                "question", "차를 살 때 가장 중요하게 생각하는 부분이 뭔가요?",
+                                "answer",
+                                "저는 차를 살 때 안전을 중요하게 생각해요. 가족들이 같이 타는 차라 항상 사고에 경각심을 갖고 있죠. 펠리세이드는 그 점에서 뒷좌석 에어백도 터지는 모델이라 안심이 되는 편이에요."
+                        )),
+                additionalQuestionOptionIds1,
+                lifestylePersonaSituationTagIds1
+        );
+        String representativePhrase2 = "매일 출퇴근하여 경제적이고 편안한 주행을 원해요.";
+        String profileImageUrl2 = "www.naver.com/profile/image.png";
+        String profileName2 = "김오토에버";
+        String profileIntroduction2 = "매일 출퇴근하는 차량은 연비가 중요하다고 생각해요.";
+        String coverImageUrl2 = "www.naver.com/cover/image.png";
+        List<Integer> additionalQuestionOptionIds2 = List.of(3, 7, 17);
+        List<Integer> lifestylePersonaSituationTagIds2 = List.of(2, 3);
+        라이프스타일_퍼소나_생성_요청(
+                representativePhrase2,
+                profileImageUrl2,
+                profileName2,
+                profileIntroduction2,
+                coverImageUrl2,
+                List.of(
+                        Map.of(
+                                "question", "어떤 용도로 차를 사용하세요?",
+                                "answer", "저는 차를 타고 출퇴근을 주로 합니다."
+                        ),
+                        Map.of(
+                                "question", "차를 살 때 가장 중요하게 생각하는 부분이 뭔가요?",
+                                "answer",
+                                "저는 차를 살 때 경제적인 점을 가장 중요시 합니다. 펠리세이드는 연비가 좋은 편이라 경제적이라고 생각합니다."
+                        )),
+                additionalQuestionOptionIds2,
+                lifestylePersonaSituationTagIds2
+        );
+
+        // WHEN
+        var response = 라이프스타일_퍼소나_전체_조회_요청();
+
+        // THEN
+        라이프스타일_퍼소나_전체_조회_응답_검증(
+                response,
+                List.of(representativePhrase1, representativePhrase2),
+                List.of(profileImageUrl1, profileImageUrl2),
+                List.of(profileName1, profileName2),
+                List.of(profileIntroduction1, profileIntroduction2),
+                List.of(coverImageUrl1, coverImageUrl2),
+                List.of(new String[]{"주행안전", "사용편의"}, new String[]{"사용편의", "추위/더위"})
+        );
     }
 }
--- backend/recommend/src/test/java/com/poongcha/recommend/acceptance/LifestylePersonaSteps.java
+++ backend/recommend/src/test/java/com/poongcha/recommend/acceptance/LifestylePersonaSteps.java
@@ -3,6 +3,7 @@ package com.poongcha.recommend.acceptance;
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
 import static util.DocumentationTest.given;
 
@@ -24,8 +25,8 @@ public class LifestylePersonaSteps {
             final String profileIntroduction,
             final String coverImageUrl,
             final List<Map> interviews,
-            final List<Long> additionalQuestionOptionIds,
-            final List<Long> lifestylePersonaSituationTagIds
+            final List<Integer> additionalQuestionOptionIds,
+            final List<Integer> lifestylePersonaSituationTagIds
     ) {
         return given()
                 .filter(document(
@@ -74,6 +75,69 @@ public class LifestylePersonaSteps {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
             assertions.assertThat(response.header(HttpHeaders.LOCATION)).isEqualTo(location);
+        }
+    }
+
+    public static ExtractableResponse<Response> 라이프스타일_퍼소나_전체_조회_요청() {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        responseFields(
+                                fieldWithPath("[].id").type(JsonFieldType.NUMBER)
+                                        .description("라이프스타일 퍼소나 ID"),
+                                fieldWithPath("[].representativePhrase").type(JsonFieldType.STRING)
+                                        .description("라이프스타일 대표 문구"),
+                                fieldWithPath("[].profile").type(JsonFieldType.OBJECT)
+                                        .description("라이프스타일 퍼소나 프로필"),
+                                fieldWithPath("[].profile.name").type(JsonFieldType.STRING)
+                                        .description("라이프스타일 퍼소나 프로필 이름"),
+                                fieldWithPath("[].profile.imageUrl").type(JsonFieldType.STRING)
+                                        .description("라이프스타일 퍼소나 프로필 이미지 URL"),
+                                fieldWithPath("[].profile.introduction").type(JsonFieldType.STRING)
+                                        .description("라이프스타일 퍼소나 소개 문구"),
+                                fieldWithPath("[].coverImageUrl").type(JsonFieldType.STRING)
+                                        .description("라이프스타일 퍼소나 표지 이미지 URL"),
+                                fieldWithPath("[].interviews").type(JsonFieldType.ARRAY)
+                                        .description("라이프스타일 퍼소나 인터뷰 목록"),
+                                fieldWithPath("[].interviews[].question").type(JsonFieldType.STRING)
+                                        .description("라이프스타일 퍼소나 인터뷰 질문"),
+                                fieldWithPath("[].interviews[].answer").type(JsonFieldType.STRING)
+                                        .description("라이프스타일 퍼소나 인터뷰 답변"),
+                                fieldWithPath("[].situationTags").type(JsonFieldType.ARRAY)
+                                        .description("라이프스타일 퍼소나 상황 태그 이름 목록")
+                        )
+                )).log().all()
+                .when()
+                .get("/lifestyle-persona")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 라이프스타일_퍼소나_전체_조회_응답_검증(
+            final ExtractableResponse<Response> response,
+            final List<String> representativePhrase,
+            final List<String> profileImageUrl,
+            final List<String> profileName,
+            final List<String> profileIntroduction,
+            final List<String> coverImageUrl,
+            final List<String[]> lifestylePersonaSituationTagNames
+    ) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+            assertions.assertThat(response.jsonPath().getList("representativePhrase")).usingRecursiveComparison()
+                    .isEqualTo(representativePhrase);
+            assertions.assertThat(response.jsonPath().getList("profile.name")).usingRecursiveComparison()
+                    .isEqualTo(profileName);
+            assertions.assertThat(response.jsonPath().getList("profile.imageUrl")).usingRecursiveComparison()
+                    .isEqualTo(profileImageUrl);
+            assertions.assertThat(response.jsonPath().getList("profile.introduction")).usingRecursiveComparison()
+                    .isEqualTo(profileIntroduction);
+            assertions.assertThat(response.jsonPath().getList("coverImageUrl")).usingRecursiveComparison()
+                    .isEqualTo(coverImageUrl);
+            assertions.assertThat(response.jsonPath().getList("situationTags[0]"))
+                    .contains(lifestylePersonaSituationTagNames.get(0));
+            assertions.assertThat(response.jsonPath().getList("situationTags[1]"))
+                    .contains(lifestylePersonaSituationTagNames.get(1));
         }
     }
 }
