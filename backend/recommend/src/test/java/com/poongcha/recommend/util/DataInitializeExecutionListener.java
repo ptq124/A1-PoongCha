b6f676ebc3762919e9d9094969d4425be08e2e13
Yunsik-Choi <choiys0311@gmail.com>
2023-08-21 14:05:56 +0900

    ⚡feat : 인수 테스트 클래스 구현

---
+++ backend/recommend/src/test/java/com/poongcha/recommend/util/DataInitializeExecutionListener.java
@@ -0,0 +1,24 @@
+package com.poongcha.recommend.util;
+
+import java.sql.PreparedStatement;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+public class DataInitializeExecutionListener extends AbstractTestExecutionListener {
+    @Override
+    public void beforeTestMethod(final TestContext testContext) {
+        ApplicationContext applicationContext = testContext.getApplicationContext();
+        NamedParameterJdbcOperations operation = applicationContext.getBean(NamedParameterJdbcOperations.class);
+    }
+
+    private static void reset(
+            final NamedParameterJdbcOperations operation,
+            final String tableName
+    ) {
+        operation.execute("set foreign_key_checks = 0;", PreparedStatement::execute);
+        operation.execute(String.format("truncate table %s;", tableName), PreparedStatement::execute);
+        operation.execute("set foreign_key_checks = 1;", PreparedStatement::execute);
+    }
+}
