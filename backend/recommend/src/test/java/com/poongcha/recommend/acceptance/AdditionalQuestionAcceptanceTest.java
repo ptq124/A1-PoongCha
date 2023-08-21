649829d78ef8f1e99f6be4489d7dc1c2e301db94
Yunsik-Choi <choiys0311@gmail.com>
2023-08-21 15:45:09 +0900

    ⛑️ test : 추가 질문 ID 조회 테스트

--- backend/recommend/src/test/java/com/poongcha/recommend/acceptance/AdditionalQuestionAcceptanceTest.java
+++ backend/recommend/src/test/java/com/poongcha/recommend/acceptance/AdditionalQuestionAcceptanceTest.java
@@ -1,9 +1,12 @@
 package com.poongcha.recommend.acceptance;
 
+import static com.poongcha.recommend.acceptance.AdditionalQuestionSteps.추가_질문_ID_조회_요청;
+import static com.poongcha.recommend.acceptance.AdditionalQuestionSteps.추가_질문_ID_조회_응답_검증;
 import static com.poongcha.recommend.acceptance.AdditionalQuestionSteps.추가_질문_생성_요청;
 import static com.poongcha.recommend.acceptance.AdditionalQuestionSteps.추가_질문_생성_응답_검증;
 
 import com.poongcha.recommend.util.RecommendAcceptanceTest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,12 +15,30 @@ public class AdditionalQuestionAcceptanceTest extends RecommendAcceptanceTest {
     @DisplayName("추가 질문 생성")
     @Test
     void 추가_질문_생성() {
+        // WHEN
         var response = 추가_질문_생성_요청(
                 "운전 경력",
                 "운전 경력이 어떻게 되시나요?",
-                new String[]{"1년 이하", "3년 이하", "5년 이상"}
+                List.of("1년 이하", "3년 이하", "5년 이상")
         );
 
+        // THEN
         추가_질문_생성_응답_검증(response, "/question/1");
+    }
+
+    @DisplayName("추가 질문 ID 조회")
+    @Test
+    void 추가_질문_ID_조회() {
+        // GIVEN
+        String topic = "운전 경력";
+        String description = "운전 경력이 어떻게 되시나요?";
+        List<String> options = List.of("1년 이하", "3년 이하", "5년 이상");
+        추가_질문_생성_요청(topic, description, options);
+
+        // WHEN
+        var response = 추가_질문_ID_조회_요청(1L);
+
+        // THEN
+        추가_질문_ID_조회_응답_검증(response, 1, topic, description, new Integer[]{1, 2, 3}, options.toArray(new String[3]));
     }
 }
--- backend/recommend/src/test/java/com/poongcha/recommend/acceptance/AdditionalQuestionSteps.java
+++ backend/recommend/src/test/java/com/poongcha/recommend/acceptance/AdditionalQuestionSteps.java
@@ -2,6 +2,9 @@ package com.poongcha.recommend.acceptance;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static util.DocumentationTest.DEFAULT_RESTDOCS_PATH;
 import static util.DocumentationTest.customRequestFields;
 import static util.DocumentationTest.given;
@@ -9,6 +12,7 @@ import static util.DocumentationTest.given;
 import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.List;
 import java.util.Map;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
@@ -26,7 +30,7 @@ public class AdditionalQuestionSteps {
     public static ExtractableResponse<Response> 추가_질문_생성_요청(
             final String topic,
             final String description,
-            final String[] additionalQuestionOptionNames
+            final List<String> additionalQuestionOptionNames
     ) {
         return given()
                 .filter(document(
@@ -34,8 +38,7 @@ public class AdditionalQuestionSteps {
                         customRequestFields(
                                 fieldWithPath("topic").type(JsonFieldType.STRING).description("질문 제목"),
                                 fieldWithPath("description").type(JsonFieldType.STRING).description("질문 설명"),
-                                fieldWithPath("options").type(JsonFieldType.ARRAY)
-                                        .description("질문 선택지")
+                                fieldWithPath("options").type(JsonFieldType.ARRAY).description("질문 선택지 목록")
                         )
                 )).log().all()
                 .when()
@@ -48,5 +51,45 @@ public class AdditionalQuestionSteps {
                 .post("/question")
                 .then().log().all()
                 .extract();
+    }
+
+    public static ExtractableResponse<Response> 추가_질문_ID_조회_요청(final long id) {
+        return given()
+                .filter(document(
+                        DEFAULT_RESTDOCS_PATH,
+                        pathParameters(
+                                parameterWithName("id").description("추가 질문 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("추가 질문 ID"),
+                                fieldWithPath("topic").type(JsonFieldType.STRING).description("질문 제목"),
+                                fieldWithPath("description").type(JsonFieldType.STRING).description("질문 설명"),
+                                fieldWithPath("options").type(JsonFieldType.ARRAY).description("질문 선택지 목록"),
+                                fieldWithPath("options[].id").type(JsonFieldType.NUMBER).description("질문 선택지 ID"),
+                                fieldWithPath("options[].name").type(JsonFieldType.STRING).description("질문 선택지 이름")
+                        )
+                )).log().all()
+                .when()
+                .get("/question/{id}", id)
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 추가_질문_ID_조회_응답_검증(
+            final ExtractableResponse<Response> response,
+            final long id,
+            final String topic,
+            final String description,
+            final Integer[] optionIds,
+            final String[] optionNames
+    ) {
+        try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
+            assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_OK);
+            assertions.assertThat(response.jsonPath().getLong("id")).isEqualTo(id);
+            assertions.assertThat(response.jsonPath().getString("topic")).isEqualTo(topic);
+            assertions.assertThat(response.jsonPath().getString("description")).isEqualTo(description);
+            assertions.assertThat(response.jsonPath().getList("options.id")).contains(optionIds);
+            assertions.assertThat(response.jsonPath().getList("options.name")).contains(optionNames);
+        }
     }
 }
