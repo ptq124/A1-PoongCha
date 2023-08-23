13f3c3d09362ba0f8400a8a15cb41eb2e889ef14
Yunsik-Choi <choiys0311@gmail.com>
2023-08-23 12:54:54 +0900

    ⛑️ test : 견적 코드 반환 테스트

--- backend/car/src/test/java/com/poongcha/car/acceptance/CarEstimateSteps.java
+++ backend/car/src/test/java/com/poongcha/car/acceptance/CarEstimateSteps.java
@@ -13,6 +13,7 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.List;
 import java.util.Map;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
 import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -53,57 +54,12 @@ public class CarEstimateSteps {
                 .extract();
     }
 
-    public static ExtractableResponse<Response> 차량_견적_ID_조회_요청(final long id) {
-        return given()
-                .filter(document(
-                        DEFAULT_RESTDOCS_PATH,
-                        responseFields(
-                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("견적 ID"),
-                                fieldWithPath("code").type(JsonFieldType.STRING).description("견적 코드"),
-                                fieldWithPath("carType.id").type(JsonFieldType.NUMBER).description("차종 ID"),
-                                fieldWithPath("carType.name").type(JsonFieldType.STRING).description("차종 이름"),
-                                fieldWithPath("carType.imageUrl").type(JsonFieldType.STRING).description("차종 이미지 URL"),
-                                fieldWithPath("trim.id").type(JsonFieldType.NUMBER).description("트림 ID"),
-                                fieldWithPath("trim.name").type(JsonFieldType.STRING).description("트림 이름"),
-                                fieldWithPath("trim.imageUrl").type(JsonFieldType.STRING).description("트림 이미지 URL"),
-                                fieldWithPath("trim.minPrice").type(JsonFieldType.NUMBER).description("트림 가격"),
-                                fieldWithPath("components[].id").type(JsonFieldType.NUMBER).description("컴포넌트 ID"),
-                                fieldWithPath("components[].name").type(JsonFieldType.STRING).description("컴포넌트 이름"),
-                                fieldWithPath("components[].additionalPrice").type(JsonFieldType.NUMBER)
-                                        .description("컴포넌트 추가 가격"),
-                                fieldWithPath("exteriorColor.id").type(JsonFieldType.NUMBER).description("외장 색상 ID"),
-                                fieldWithPath("exteriorColor.name").type(JsonFieldType.STRING).description("외장 색상 이름"),
-                                fieldWithPath("exteriorColor.imageUrl").type(JsonFieldType.STRING)
-                                        .description("외장 색상 이미지 URL"),
-                                fieldWithPath("exteriorColor.type").type(JsonFieldType.STRING).description("외장 색상 타입"),
-                                fieldWithPath("interiorColor.id").type(JsonFieldType.NUMBER).description("내장 색상 ID"),
-                                fieldWithPath("interiorColor.name").type(JsonFieldType.STRING).description("내장 색상 이름"),
-                                fieldWithPath("interiorColor.imageUrl").type(JsonFieldType.STRING)
-                                        .description("내장 색상 이미지 URL"),
-                                fieldWithPath("interiorColor.type").type(JsonFieldType.STRING).description("내장 색상 타입"),
-                                fieldWithPath("optionGroups").type(JsonFieldType.ARRAY).description("옵션 그룹 목록"),
-                                fieldWithPath("optionGroups[].id").type(JsonFieldType.NUMBER).description("옵션 그룹 ID"),
-                                fieldWithPath("optionGroups[].name").type(JsonFieldType.STRING).description("옵션 그룹 이름"),
-                                fieldWithPath("optionGroups[].additionalPrice").type(JsonFieldType.NUMBER)
-                                        .description("옵션 그룹 추가 가격"),
-                                fieldWithPath("optionGroups[].options[].id").type(JsonFieldType.NUMBER)
-                                        .description("옵션 ID"),
-                                fieldWithPath("optionGroups[].options[].name").type(JsonFieldType.STRING)
-                                        .description("옵션 이름"),
-                                fieldWithPath("optionGroups[].options[].imageUrl").type(JsonFieldType.STRING)
-                                        .description("옵션 이미지 URL")
-                        )
-                ))
-                .log().all()
-                .when()
-                .get("/estimate/{id}", id)
-                .then().log().all()
-                .extract();
-    }
-
     public static void 차량_견적_생성_응답_검증(final ExtractableResponse<Response> response) {
         try (AutoCloseableSoftAssertions assertions = new AutoCloseableSoftAssertions()) {
             assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.SC_CREATED);
+            String location = response.header(HttpHeaders.LOCATION);
+            String estimateCode = location.substring(location.lastIndexOf("/") + 1);
+            assertions.assertThat(response.jsonPath().getString("code")).isEqualTo(estimateCode);
         }
     }
 
